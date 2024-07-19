0203f6488d5f61550e4eb2332e2db8b85f868c56
Kerem Yardan <keremyrdn@hotmail.com>
2024-07-19 23:07:33 +0300

    js clock created

---
+++ js/clock.js
@@ -0,0 +1,25 @@
+
+window.onload = function() {
+    const username = prompt("Enter your name: ");
+
+    if (username) {
+       
+        document.getElementById("myName").innerText = username;
+    } else {
+        
+        alert("Enter your name");
+    }
+
+    function showTime() {
+        const now = new Date();
+        const hours = now.getHours().toString().padStart(2, '0');
+        const minutes = now.getMinutes().toString().padStart(2, '0');
+        const seconds = now.getSeconds().toString().padStart(2, '0');
+        const formattedTime = `${hours}:${minutes}:${seconds}`;
+        document.getElementById("myClock").innerText = formattedTime;
+
+
+    }
+            showTime();
+        setInterval(showTime,1000);
+};
